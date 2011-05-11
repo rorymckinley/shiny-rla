1a8b1e7b5697b7b2d7ada7849bbb7d5ad2d7b547
Rory McKinley <rorymckinley@gmail.com>
2011-05-11 16:00:24 +0200

    Initial commit - empty Rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShinyRla::Application.config.secret_token = 'ad8a604f7050443a6f25acde47c42ea41958c10a7d0d8067bc39d97245da975500c7e2cbe52bde53d7e934730c38d050340f9ffa16f9be1ae22ff46556e6f07c'
